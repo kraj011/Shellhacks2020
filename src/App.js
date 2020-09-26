227ec9095a49c71ba8d7bde4d2fa1cb167efc50f
David Krajewski <davidkrajewski@yahoo.com>
2020-09-26 00:22:14 -0400

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,62 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import TwitterLogin from "react-twitter-login";
+const App = () => {
+	const [user, setUser] = useState({});
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+	useEffect(() => {
+		const userLocal = localStorage.getItem("user");
+
+		if (userLocal !== null) {
+			setUser(JSON.parse(userLocal));
+		}
+
+		return () => {};
+	}, []);
+
+	useEffect(() => {
+		/**
+		 * Load in tweets whenever a user object is loaded
+		 */
+		if (!user.screen_name) {
+			return;
+		}
+
+		fetch("http://localhost:3001/getTweets", { method: "POST" }).then(
+			(res) => {
+				console.log(res);
+			}
+		);
+	}, [user]);
+
+	const authHandler = (err, data) => {
+		if (err) {
+			alert(err);
+			return;
+		}
+		setUser(data);
+		localStorage.setItem("user", JSON.stringify(data));
+	};
+
+	return (
+		<div className="App">
+			<header className="App-header">
+				{user.screen_name ? (
+					<h1>Welcome back, {user.screen_name}</h1>
+				) : (
+					<TwitterLogin
+						authCallback={authHandler}
+						consumerKey={"bq7ycdnw2f4qwViP6RYHs4Ppm"}
+						consumerSecret={
+							"h0JGaWXSlvIlZKvWhQ54dMO27o0jI7UFas9V59v6BLdYf2xLv3"
+						}
+						callbackUrl={"http://localhost:3000/"}
+					/>
+				)}
+			</header>
+		</div>
+	);
+};
 
 export default App;
